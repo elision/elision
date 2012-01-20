82d5c11a156364fc5621f65a069220b5ae11deb4
Prowell, Stacy <prowellsj@ornl.gov>
2012-01-20 01:55:52 -0500

    Added new atom parser and a simple rewrite rule class.

--- Elision/src/sjp/elision/core/AtomParser.scala
+++ Elision/src/sjp/elision/core/AtomParser.scala
@@ -10,222 +10,300 @@ package sjp.elision.core
 
 import org.parboiled.scala._
 import org.parboiled.errors.{ ErrorUtils, ParsingException }
+import scala.collection.mutable.LinkedList
+
+sealed abstract class AstNode {
+  def interpret: BasicAtom
+}
+case class AtomList(list: LinkedList[AstNode]) {
+  def interpret = AtomList(list)
+}
+case class UnsignedInteger(digits: String, radix: Int) {
+  def interpret = Literal(INTEGER, Integer.parseInt(digits, radix))
+}
+case class SignedInteger(sign: Boolean, digits: String, radix: Int) {
+  def interpret = Literal(INTEGER,
+      if (sign) Integer.parseInt(digits, radix) 
+      else -Integer.parseInt(digits, radix))
+}
+object SignedInteger {
+	def apply(sign: Option[Boolean], integer: UnsignedInteger): SignedInteger =
+		sign match {
+		case Some(bool) => SignedInteger(bool, integer.digits, integer.radix)
+		case None => SignedInteger(true, integer.digits, integer.radix)
+	}
+}
 
 /**
  * A parser to parse a single atom.
  */
 class AtomParser extends Parser {
-  def parseAtom(atom: String) = {
-    val run = TracingParseRunner(Atom).run(atom)
-    println(run.result)
-    /*
+	def parseAtom(atom: String) = {
+		val run = TracingParseRunner(Atom).run(atom)
+		println(run.result)
+		/*
     run.result match {
       case Some(parsedAtom) => parsedAtom.toString
       case None => throw new ParsingException("Invalid atom.\n" +
           ErrorUtils.printParseErrors(run))
     }
-    */
-  }
+		 */
+	}
 
-  def show(x: String) = println("\n++++++++++ " + x + " ++++++++++\n")
+	def show(x: String) = println("\n++++++++++ " + x + " ++++++++++\n")
 
-  //======================================================================
-  // Parse and build atoms.
-  //======================================================================
-  
-  def Atom: Rule0 = rule {
-    zeroOrMore(Whitespace ~ (
-        // Parse an operator application.  This just applies an operator to
-        // some other atom.  The operator name is given as a symbol, and the
-        // argument may be enclosed in parens if it is a list, or joined to
-        // the operator with a dot if not.
-        ESymbol ~ "." ~ Atom |
-        ESymbol ~ "(" ~ AtomList ~ ")" |
-        
-        // Parse an atom list that specifies its properties.  There are
-        // multiple different forms of lists.  The associativity or
-        // commutativity of the list is specified by writing a percent sign and
-        // an A (for associativity) or C (for commutativity), followed by the
-        // list in parens.
-        // Note that f(x,y,z), if f is associative, could be written as:
-        // f.%A(x,y,z)
-        "%" ~ ("AC" | "CA" | "C" | "A") ~ "(" ~ AtomList ~ ")" |
-        
-        // Parse a variable.  The leading dollar sign is used to distinguish
-        // between a symbol and a variable.  If a type is not specified for a
-        // variable, it gets put in the type universe.
-        Variable |
-        
-        // A "naked" operator is specified by explicitly giving the operator
-        // type OPTYPE.  Otherwise it is parsed as a symbol.
-        ESymbol ~ ":" ~ "OPTYPE" |
-        
-        // Parse a literal.  A literal can take many forms, but it should be
-        // possible to always detect the kind of literal during parse.  By
-        // default literals go into a simple type, but this can be overridden.
-        ESymbol ~ ":" ~ Atom |
-        ESymbol |
-        EString ~ ":" ~ Atom |
-        EString |
-        AnyNumber ~ ":" ~ Atom |
-        AnyNumber |
-        
-        // Parse the special type universe.
-        "^TYPE"
-      ) ~% (show)
-    ) ~ EOI
-  }
-  
-  /**
-   * Parse a list of atoms, separated by commas.  No concept of associativity,
-   * commutativity, etc., is inferred at this point.
-   */
-  def AtomList = rule {
-    Atom ~ zeroOrMore("," ~ Atom)
-  }
-  
-  /**
-   * Parse a rule.
-   */
-  def Rule = rule (
-    // First there are optional variable declarations.  In a rule, all pattern
-    // variables must be declared.
-    zeroOrMore("@" ~ Variable ~ zeroOrMore("," ~ Variable)) ~
-    
-    // Next is the rule itself, consisting of a pattern, a rewrite, and zero
-    // or more guards.
-    ignoreCase("rule") ~ Atom ~ "->" ~ Atom ~ zeroOrMore("if" ~ Atom) ~
-    
-    // Next the rule can be declared to be in zero or more rulesets.
-    zeroOrMore((ignoreCase("rulesets") | ignoreCase("ruleset")) ~ ESymbol) ~
-    
-    // Finally the rule's cache level can be declared.  This must be the
-    // last item, if present.
-    optional("level" ~ DInteger)
-  )
-  
-  /**
-   * Parse a variable.
-   */
-  def Variable = rule {
-    "$" ~ ESymbol ~ ":" ~ Atom |
-    "$" ~ ESymbol
-  }
+	//======================================================================
+	// Parse and build atoms.
+	//======================================================================
 
-  //======================================================================
-  // Parse whitespace.
-  //======================================================================
+	def Atom: Rule1[BasicAtom] = rule {
+		Whitespace ~ (
+				// Parse the special OPTYPE.
+				"OPTYPE" ~> (x => OPTYPE) |
+		    
+		    // Parse the special RULETYPE.
+		    "RULETYPE" ~> (x => RULETYPE) |
+		    
+		    // Parse a "typical" operator application.
+		    OperatorApplication |
+		    
+		    // Parse a typed list.
+		    TypedList |
 
-  /** Parse ignorable whitespace. */
-  def Whitespace = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
+				// Parse a variable.  The leading dollar sign is used to distinguish
+				// between a symbol and a variable.  If a type is not specified for a
+				// variable, it gets put in the type universe.
+				Variable |
 
-  //======================================================================
-  // Parse a string.
-  //======================================================================
+				// A "naked" operator is specified by explicitly giving the operator
+				// type OPTYPE.  Otherwise it is parsed as a symbol.
+				ESymbol ~ ":" ~ "OPTYPE" |
 
-  /** Parse a double-quoted string. */
-  def EString = rule {
-    "\"" ~ zeroOrMore(Character) ~ "\""
-  }
+				// Parse a literal.  A literal can take many forms, but it should be
+				// possible to always detect the kind of literal during parse.  By
+				// default literals go into a simple type, but this can be overridden.
+				ESymbol ~ ":" ~ Atom |
+				ESymbol |
+				EString ~ ":" ~ Atom |
+				EString |
+				AnyNumber ~ ":" ~ Atom |
+				AnyNumber |
+				
+				// Parse the special type universe.
+				"^TYPE"
+		) ~% (show)
+	}
 
-  /** Parse a character in a string. */
-  def Character = rule { EscapedCharacter | NormalCharacter }
+	def OperatorApplication: Rule1[AstNode] = rule {
+	  		// Parse an operator application.  This just applies an operator to
+				// some other atom.  The operator name is given as a symbol, and the
+				// argument may be enclosed in parens if it is a list, or joined to
+				// the operator with a dot if not.
+				ESymbol ~ "." ~ Atom |
+				ESymbol ~ "(" ~ AtomList ~ ")"
+	}
+	
+	def TypedList = rule {
+	  		// Parse an atom list that specifies its properties.  There are
+				// multiple different forms of lists.  The associativity or
+				// commutativity of the list is specified by writing a percent sign and
+				// an A (for associativity) or C (for commutativity), followed by the
+				// list in parens.
+				// Note that f(x,y,z), if f is associative, could be written as:
+				// f.%A(x,y,z)
+				"%" ~ ("AC" | "CA" | "C" | "A") ~ "(" ~ AtomList ~ ")"
+	}
+	
+	/**
+	 * Parse a list of atoms, separated by commas.  No concept of associativity,
+	 * commutativity, etc., is inferred at this point.
+	 */
+	def AtomList: LinkedList[BasicAtom] = rule {
+			var list = new LinkedList[BasicAtom]
+			                          Atom ~> { list :+ _ } ~
+			                          zeroOrMore("," ~
+			                          		Atom ~> { list :+ _ }
+			                          ) ~~> ()
+		}
 
-  /** Parse an escaped character. */
-  def EscapedCharacter = rule { "\\" ~ anyOf("\"\\nrt") }
+		/**
+		 * Parse a rule.
+		 */
+		def Rule = rule (
+				// First there are optional variable declarations.  In a rule, all pattern
+				// variables must be declared.
+				zeroOrMore("@" ~ Variable ~ zeroOrMore("," ~ Variable)) ~
 
-  /** Parse a normal character. */
-  def NormalCharacter = rule { noneOf("\"\\") ~ ANY }
+				// Next is the rule itself, consisting of a pattern, a rewrite, and zero
+				// or more guards.
+				ignoreCase("rule") ~ Atom ~ "->" ~ Atom ~ zeroOrMore("if" ~ Atom) ~
 
-  //======================================================================
-  // Parse a symbol.
-  //======================================================================
+				// Next the rule can be declared to be in zero or more rulesets.
+				optional((ignoreCase("rulesets") | ignoreCase("ruleset")) ~ ESymbol ~
+						zeroOrMore("," ~ ESymbol)) ~
 
-  /** Parse a symbol. */
-  def ESymbol = rule {
-    "`" ~ zeroOrMore(SymChar) ~ "`" |
-      (("a" - "z" | "A" - "Z" | "_") ~ zeroOrMore(
-        "a" - "z" | "A" - "Z" | "0" - "9" | "_"))
-  }
-  
-  def SymChar = rule { SymEsc | SymNorm }
-  def SymEsc = rule { "\\" ~ anyOf("`\\nrt") }
-  def SymNorm = rule { noneOf("`\\") ~ ANY }
+						// Finally the rule's cache level can be declared.  This must be the
+						// last item, if present.
+						optional("level" ~ DInteger)
+		)
 
-  //======================================================================
-  // Parse a number.
-  //======================================================================
+		/**
+		 * Parse a variable.
+		 */
+		def Variable: Rule1[BasicAtom] = rule {
+			"$" ~ ESymbol ~ ":" ~ Atom ~~> (
+			    (name: String, typ: BasicAtom) => Variable(typ, Symbol(name))) |
+			"$" ~ ESymbol ~~> (sval => Variable(TypeUniverse, Symbol(sval)))
+		}
 
-  /* Numbers are parsed in a somewhat unusual way.  We parse the number, and
-   * make a decision later if the number is a float or integer based on what
-   * we find.
-   */
+		//======================================================================
+		// Parse whitespace.
+		//======================================================================
 
-  def AnyNumber = rule {
-    optional("-") ~
-      (HNumber |
-        BNumber |
-        DNumber |
-        ONumber)
-  }
+		/** Parse ignorable whitespace. */
+		def Whitespace = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
 
-  def HNumber = rule {
-    HInteger ~ optional("." ~ zeroOrMore(HDigit)) ~ optional(Exponent)
-  }
+		//======================================================================
+		// Parse a string.
+		//======================================================================
 
-  def BNumber = rule {
-    BInteger ~ optional("." ~ zeroOrMore(BDigit)) ~ optional(Exponent)
-  }
+		/** Parse a double-quoted string. */
+		def EString = rule {
+			"\"" ~ zeroOrMore(Character) ~> (_.toString) ~ "\""
+		}
 
-  def DNumber = rule {
-    DInteger ~ optional("." ~ zeroOrMore(DDigit)) ~ optional(Exponent)
-  }
+		/** Parse a character in a string. */
+		def Character = rule { EscapedCharacter | NormalCharacter }
 
-  def ONumber = rule {
-    OInteger ~ optional("." ~ zeroOrMore(ODigit)) ~ optional(Exponent)
-  }
+		/** Parse an escaped character. */
+		def EscapedCharacter = rule { "\\" ~ anyOf("\"\\nrt") }
 
-  def Exponent = rule {
-    (ignoreCase("e") | ignoreCase("p")) ~ optional("+" | "-") ~ AnyInteger
-  }
+		/** Parse a normal character. */
+		def NormalCharacter = rule { noneOf("\"\\") ~ ANY }
 
-  def AnyInteger = rule {
-    HInteger |
-      BInteger |
-      DInteger |
-      OInteger
-  }
+		//======================================================================
+		// Parse a symbol.
+		//======================================================================
 
-  def HInteger = rule {
-    ignoreCase("0x") ~ oneOrMore(HDigit)
-  }
+		/** Parse a symbol. */
+		def ESymbol = rule {
+			"`" ~ zeroOrMore(SymChar) ~> (_.toString) ~ "`" |
+			(("a" - "z" | "A" - "Z" | "_") ~ zeroOrMore(
+					"a" - "z" | "A" - "Z" | "0" - "9" | "_")) ~> (_.toString)
+		}
 
-  def BInteger = rule {
-    ignoreCase("0b") ~ oneOrMore(BDigit)
-  }
+		def SymChar = rule { SymEsc | SymNorm }
+		def SymEsc = rule { "\\" ~ anyOf("`\\nrt") }
+		def SymNorm = rule { noneOf("`\\") ~ ANY }
 
-  def DInteger = rule {
-    ("1" - "9") ~ zeroOrMore(DDigit)
-  }
+		//======================================================================
+		// Parse a number.
+		//======================================================================
 
-  def OInteger = rule {
-    "0" ~ zeroOrMore(ODigit)
-  }
+		/* Numbers are parsed in a somewhat unusual way.  We parse the number, and
+		 * make a decision later if the number is a float or integer based on what
+		 * we find.
+		 */
 
-  def DDigit = rule { "0" - "9" }
-  def ODigit = rule { "0" - "7" }
-  def HDigit = rule { "0" - "9" | "a" - "f" | "A" - "F" }
-  def BDigit = rule { "0" | "1" }
+		def AnyNumber = rule {
+			optional("-") ~
+			(HNumber |
+					BNumber |
+					DNumber |
+					ONumber)
+		}
 
-  //======================================================================
-  // Other methods affecting the parse.
-  //======================================================================
+		def HNumber = rule {
+			HInteger ~
+			optional("." ~ zeroOrMore(HDigit) ~> (UnsignedInteger(_,16))) ~
+			optional(Exponent)
+		}
 
-  /**
-   * Eliminate trailing whitespace.  This trick is found on the Parboiled web
-   * site in the examples.
-   */
-  override implicit def toRule(string: String) =
-    if (string.endsWith(" ")) str(string.trim) ~ Whitespace
-    else str(string)
+		def BNumber = rule {
+			BInteger ~
+			optional("." ~ zeroOrMore(BDigit) ~> (UnsignedInteger(_,2))) ~
+			optional(Exponent)
+		}
+
+		def DNumber = rule {
+			DInteger ~
+			optional("." ~ zeroOrMore(DDigit) ~> (UnsignedInteger(_,10))) ~
+			optional(Exponent)
+		}
+
+		def ONumber = rule {
+			OInteger ~
+			optional("." ~ zeroOrMore(ODigit) ~> (UnsignedInteger(_,8))) ~
+			optional(Exponent)
+		}
+
+		/**
+		 * Parse an exponent expression.
+		 * @return	A signed integer.
+		 */
+		def Exponent = rule {
+			(ignoreCase("e") | ignoreCase("p")) ~
+			optional("+" ~ push(true) | "-" ~ push(false)) ~
+			AnyInteger ~~> (SignedInteger(_,_))
+		}
+
+		/**
+		 * Parse an integer.
+		 * @return	An unsigned integer.
+		 */
+		def AnyInteger = rule {
+			HInteger |
+			BInteger |
+			DInteger |
+			OInteger
+		}
+
+		/**
+		 * Parse a hexadecimal integer.
+		 * @return	An unsigned integer.
+		 */
+		def HInteger = rule {
+			ignoreCase("0x") ~ oneOrMore(HDigit) ~> (UnsignedInteger(_:String,16))
+		}
+
+		/**
+		 * Parse a binary integer.
+		 * @return	An unsigned integer.
+		 */
+		def BInteger = rule {
+			ignoreCase("0b") ~ oneOrMore(BDigit) ~> (UnsignedInteger(_:String,2))
+		}
+
+		/**
+		 * Parse a decimal integer.
+		 * @return	An unsigned integer.
+		 */
+		def DInteger = rule {
+			(("1" - "9") ~ zeroOrMore(DDigit)) ~> (UnsignedInteger(_:String,10))
+		}
+
+		/**
+		 * Parse an octal integer.
+		 * @return	An unsigned integer.
+		 */
+		def OInteger = rule {
+			("0" ~ zeroOrMore(ODigit)) ~> (UnsignedInteger(_:String,8))
+		}
+
+		def DDigit = rule { "0" - "9" }
+		def ODigit = rule { "0" - "7" }
+		def HDigit = rule { "0" - "9" | "a" - "f" | "A" - "F" }
+		def BDigit = rule { "0" | "1" }
+
+		//======================================================================
+		// Other methods affecting the parse.
+		//======================================================================
+
+		/**
+		 * Eliminate trailing whitespace.  This trick is found on the Parboiled web
+		 * site in the examples.
+		 */
+		override implicit def toRule(string: String) =
+			if (string.endsWith(" ")) str(string.trim) ~ Whitespace
+			else str(string)
 }
---
+++ Elision/src/sjp/elision/core/RewriteRule.scala
@@ -0,0 +1,57 @@
+/* Copyright (c) 2012 by Stacy Prowell (sprowell@gmail.com).
+ * All rights reserved.  http://stacyprowell.com
+ *       _ _     _
+ *   ___| (_)___(_) ___  _ __
+ *  / _ \ | / __| |/ _ \| '_ \
+ * |  __/ | \__ \ | (_) | | | |
+ *  \___|_|_|___/_|\___/|_| |_|
+ */
+package sjp.elision.core
+
+/**
+ * A type for all rules.
+ */
+object RULETYPE extends RootType {
+  val theType = TypeUniverse
+  
+  override def toString = "RULETYPE"
+}
+
+/**
+ * Encapsulate a rewrite rule.
+ */
+case class RewriteRule(pattern: BasicAtom, rewrite: BasicAtom,
+    guards: Seq[BasicAtom], rulesets: Set[String], cacheLevel: Int)
+    extends BasicAtom {
+  val theType = RULETYPE
+
+  def tryMatchWithoutTypes(subject: BasicAtom, binds: Bindings) =
+    // Rules match only other rules.
+    subject match {
+    case RewriteRule(opat, orew, ogua, orul, olev) =>
+      Fail("Rule matching is not implemented.", this, subject)
+    case _ => Fail("Rules cannot match non-rules.", this, subject)
+  }
+
+  def rewrite(binds: Bindings) = {
+    // Rewrite the pattern and rewrite.
+    val (newpat, patchanged) = pattern.rewrite(binds)
+    val (newrew, rewchanged) = rewrite.rewrite(binds)
+    // Rewrite all the guards.
+    var changed = patchanged || rewchanged
+    var newgua = guards.map(gua => {
+      val (newgua, guachanged) = gua.rewrite(binds)
+      changed |= guachanged
+      newgua
+    })
+    if (changed)
+      (RewriteRule(newpat, newrew, newgua, rulesets, cacheLevel), true)
+    else (this, false)
+  }
+  
+  override def toString = "RULE " + pattern.toString + " -> " +
+  	rewrite.toString +
+  	(if (!guards.isEmpty) guards.mkString(" if ", " if ", "") else "") +
+  	(if (!rulesets.isEmpty) rulesets.mkString(" rulesets ", ", ", "") else "") +
+  	" level " + cacheLevel
+}
