a9fc870f3997b392c8906370cdb209e6ee8da8da
Prowell, Stacy <prowellsj@ornl.gov>
2012-03-07 17:20:23 -0500

    Starting on a Generator and the matching implementation.

---
+++ Elision/src/sjp/elision/core/matcher/Chooser.scala
@@ -0,0 +1,43 @@
+/*       _ _     _
+ *   ___| (_)___(_) ___  _ __
+ *  / _ \ | / __| |/ _ \| '_ \
+ * |  __/ | \__ \ | (_) | | | |
+ *  \___|_|_|___/_|\___/|_| |_|
+ *
+ * Copyright (c) 2012 by Stacy Prowell (sprowell@gmail.com).
+ * All rights reserved.  http://stacyprowell.com
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package sjp.elision.core.matcher
+
+/**
+ * Iterate over all choices of a fixed number of object from a larger
+ * collection of objects.
+ * 
+ * A ''choice'' is an unordered collection of ''m'' objects from some
+ * collection of ''n'' objects, where ''n''>=''m''.  This is typically written
+ * as ''n''C''m'', and there are ''n''! / ''m''! / (''n''-''m'')! ways to make
+ * this choice.
+ */
+class Chooser {
+
+}
