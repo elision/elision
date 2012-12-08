482d6379351b833bdf29f4f8231e33322a519709
Prowell, Stacy J <prowellsj@ornl.gov>
2012-12-07 20:14:22 -0500

    Moved ElisionException to the util package.

--- Elision/src/ornl/elision/util/ElisionException.scala
+++ Elision/src/ornl/elision/util/ElisionException.scala
@@ -34,7 +34,7 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ======================================================================*/
-package ornl.elision
+package ornl.elision.util
 
 /**
  * Root class for exceptions occurring within Elision.  These may be used
