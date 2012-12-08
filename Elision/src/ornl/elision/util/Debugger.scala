6853716f7e9a5a54cf4a787d6088f63e6d0f773d
Prowell, Stacy J <prowellsj@ornl.gov>
2012-12-07 20:14:02 -0500

    Moved the debugger class to the util package.

--- Elision/src/ornl/elision/util/Debugger.scala
+++ Elision/src/ornl/elision/util/Debugger.scala
@@ -27,7 +27,9 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package ornl.elision
+package ornl.elision.util
+
+import ornl.elision.core.Timeable
 
 /**
  * This is a class for managing and printing debug output during execution
