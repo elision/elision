b9efb1314949d446db79a1f3c975c74d1d52f8d6
Prowell, Stacy J <prowellsj@ornl.gov>
2013-02-24 01:07:04 -0500

    Mods to travis.sh script.

--- travis.sh
+++ travis.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env sh
 #
 # Build for Travis
 #
 
+echo "Found the travis.sh script, and executing."
 cd  $CI_HOME/Elision
+echo "Switched directory to the Elision project."
 sbt ++$TRAVIS_SCALA_VERSION package
+echo "Completed the build using sbt."
 
