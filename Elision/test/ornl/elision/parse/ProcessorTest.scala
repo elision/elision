0c1d78c4668f04e17a8eec4060c6e3d80285f8f3
jblack1395 <planiturthian@gmail.com>
2013-04-11 12:29:22 -0400

    Removed Bitstring.eli from Boot.eli, which needs to be added back for the unit test to work.  Fixed ProcessorTest to work with the ERepl changes.

--- Elision/test/ornl/elision/parse/ProcessorTest.scala
+++ Elision/test/ornl/elision/parse/ProcessorTest.scala
@@ -37,6 +37,8 @@ import org.junit.Before
 import ornl.elision.repl.ERepl
 import ornl.elision.repl.ReplMain
 import ornl.elision.cli.CLI
+import scala.collection.mutable.HashMap
+import ornl.elision.cli.CLI.CLIState
 
 /**
  * @author jb9
@@ -48,7 +50,16 @@ class ProcessorTest extends AssertionsForJUnit {
    */
   @Test 
   def testParse() {
-    val test = new ERepl(CLI.CLIState())
+  val cli = ReplMain.prep(Array(""))
+  var settings = new HashMap[String, String]
+  settings("elision.root") = sys.env("FX_DIR") + java.io.File.separator + "config" +
+    java.io.File.separator + "elision"
+  settings("elision.history") = "history"
+  settings("elision.context") = "context"
+  settings("elision.rc") = "rc"
+  settings("elision.cache") = "cache"
+
+  val test = new ERepl(CLIState(List(), settings.toMap))
     ornl.elision.core.knownExecutor = test
     test.bootstrap(0)
     test.parse("(UnitTest)", """inc("C:\\Users\\jb9\\config\\files_inc.eli")""")
