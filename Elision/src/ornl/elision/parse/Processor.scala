c61470bf10abb1ee2803a3f26a537fa8e112f111
David Heise <heiseda@ornl.gov>
2013-09-20 14:38:22 -0400

    Made the Processor.read() interface consistent across all overloads. Moved success check to the read() overload called by all other overloads.

--- Elision/src/ornl/elision/parse/Processor.scala
+++ Elision/src/ornl/elision/parse/Processor.scala
@@ -184,7 +184,7 @@ with HasHistory {
    * @param filename		The file to read.  It may be absolute, or it may be
    * 										relative to the current directory.
    * @param quiet       If true, do not emit any error messages.
-   * @return  True if the file was found; false if it was not.
+   * @return  True if the file was found and parse was successful; false if it was not.
    */
   def read(filename: String, quiet: Boolean): Boolean = {
     // Make a resolver from the properties.  Is this costly to do every time
@@ -193,10 +193,11 @@ with HasHistory {
     val useClassPath = getProperty[Boolean]("useclasspath")
     val path = getProperty[String]("path")
     val resolver = FileResolver(usePath, useClassPath, Some(path))
+    var result = false
     resolver.find(filename) match {
       case None =>
         if (!quiet) console.error("File not found: " + filename)
-        false
+        result = false
         
       case Some((reader, dir)) =>
         Processor.fileReadStack.push(filename)
@@ -209,17 +210,13 @@ with HasHistory {
         }
         
         // Proceed with reading the file's stream.
-        val result = read(scala.io.Source.fromInputStream(reader), filename)
+        result = read(scala.io.Source.fromInputStream(reader), filename)
         
         // Restore our original path.
         setProperty[String]("path", path)
         Processor.fileReadStack.pop
-        result match {
-          case r : Success => true
-          case r : Failure => false
-          case _ => false
-        }
     }
+    result
   }
   
   /**
@@ -229,7 +226,7 @@ with HasHistory {
    * @throws	java.io.IOException
    * 					The file cannot be found or cannot be read.
    */
-  def read(file: java.io.File) : Presult = {
+  def read(file: java.io.File) : Boolean = {
     read(scala.io.Source.fromFile(file), file.getAbsolutePath)
   }
   
@@ -241,8 +238,11 @@ with HasHistory {
    * @throws	java.io.IOException
    * 					An error occurred trying to read.
    */
-  def read(source: scala.io.Source, filename: String = "(console)") : Presult = {
-    _execute(_makeParser(filename).parseAtoms(source), true) 
+  def read(source: scala.io.Source, filename: String = "(console)") : Boolean = {
+    _execute(_makeParser(filename).parseAtoms(source), true) match {
+      case r : Success => true
+      case r : Failure => false
+    }
   }
   
   /**
